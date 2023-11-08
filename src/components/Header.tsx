f6963846d67c4338eac680b7e3afeb7b59fd0f07
Maks <mpasiecki@gmail.com>
2023-11-08 00:18:37 +0000

    update assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
-import logo from "../assets/logo.png";
+import logo from "../assets/photos/logo.png";
 import { scrollTo } from "../utils/utils";
 import { useState } from "react";
 
